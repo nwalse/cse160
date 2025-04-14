ff334b9df7978485f7c17c9e43a66088f49b2a0f
neelwalse <117557589+nwalse@users.noreply.github.com>
2025-04-13 18:12:08 -0700

    part 7 done

--- asg1/src/ColoredPoints.js
+++ asg1/src/ColoredPoints.js
@@ -76,6 +76,10 @@ function addActionsForHtmlUI() {
   document.getElementById('red').onclick = function() { 
     g_selectedColor = [1.0,0.0,0.0,1.0]; 
   };
+  document.getElementById('clear').onclick = function() { 
+    g_shapesList = [];
+    renderAllShapes();
+  };
 
   document.getElementById('redSlide').addEventListener('mouseup', function () { g_selectedColor[0] = this.value/100 });
   document.getElementById('greenSlide').addEventListener('mouseup', function () { g_selectedColor[1] = this.value/100 });
