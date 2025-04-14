af7cc6733dac615264755a8a74580fc4dfc85afb
neelwalse <117557589+nwalse@users.noreply.github.com>
2025-04-13 18:08:22 -0700

    part 6 done

--- asg1/src/ColoredPoints.js
+++ asg1/src/ColoredPoints.js
@@ -102,19 +102,17 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-var g_sizes = [];
+var g_shapesList = [];
 
 function click(ev) {
   [x, y] = convertCoordinates(ev);
 
   // Store the coordinates to g_points array
-  g_points.push([x, y]);
-
-  g_colors.push(g_selectedColor.slice());
-
-  g_sizes.push(g_selectedSize);
+  var point = new Point();
+  point.position = [x, y, 0.0];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
 
   renderAllShapes();
 
@@ -135,18 +133,9 @@ function renderAllShapes() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  var len = g_shapesList.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-    var size = g_sizes[i];
+    g_shapesList[i].render();
 
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    gl.uniform1f(u_Size, size);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
   }
 }
---
+++ asg1/src/Point.js
@@ -0,0 +1,15 @@
+class Point {
+  constructor() {
+    this.type = 'Point';
+    this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0, 1.0, 1.0, 1.0];
+    this.size = 5;
+  }
+
+  render() {
+    gl.vertexAttrib3f(a_Position, this.position[0], this.position[1], 0.0);
+    gl.uniform4f(u_FragColor, this.color[0], this.color[1], this.color[2], this.color[3]);
+    gl.uniform1f(u_Size, this.size);
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
+}
