9cb0262f2ec7e38fd7e723ff00d3e0e53c506698
neelwalse <117557589+nwalse@users.noreply.github.com>
2025-04-27 16:38:47 -0700

    helpers

--- asg2/src/Cube.js
+++ asg2/src/Cube.js
@@ -1,32 +1,52 @@
-class Circle {
+class Cube {
   constructor() {
-    this.type = "circle";
-    this.position = [0.0, 0.0, 0.0];
-    this.color = [1.0, 1.0, 1.0, 1.0];
-    this.size = 5.0;
-    this.segments = 10;
+    this.type = 'cube';
+    this.color = [0.5, 0.5, 0.5, 0.5];
+    this.matrix = new Matrix4();
+
+    this.buffer = null;
   }
 
   render() {
-    var xy = this.position;
+    if(this.buffer === null) {
+      this.buffer = gl.createBuffer();
+      if (!this.buffer) {
+        console.log("Failed to create the buffer object");
+        return -1;
+      }
+    }
+
     var rgba = this.color;
 
+    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+    // front of the cube
+    gl.uniform4f(u_FragColor, 0.9*rgba[0], 0.9*rgba[1], 0.9*rgba[2], rgba[3]);
+    drawTriangle3D([0.0, 0.0, 0.0,  1.0, 1.0, 0.0,  1.0, 0.0, 0.0], this.buffer);
+    drawTriangle3D([0.0, 0.0, 0.0,  0.0, 1.0, 0.0,  1.0, 1.0, 0.0], this.buffer);
+
+    // face to the right
+    gl.uniform4f(u_FragColor, 0.75*rgba[0], 0.75*rgba[1], 0.75*rgba[2], rgba[3]);
+    drawTriangle3D([1.0, 0.0, 0.0,  1.0, 1.0, 1.0,  1.0, 0.0, 1.0], this.buffer);
+    drawTriangle3D([1.0, 0.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 1.0], this.buffer);
+
+    // face to the left
+    drawTriangle3D([0.0, 0.0, 0.0,  0.0, 1.0, 1.0,  0.0, 0.0, 1.0], this.buffer);
+    drawTriangle3D([0.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 1.0], this.buffer);
+
+    // top face
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    drawTriangle3D([0.0, 1.0, 0.0,  1.0, 1.0, 1.0,  1.0, 1.0, 0.0], this.buffer);
+    drawTriangle3D([0.0, 1.0, 0.0,  0.0, 1.0, 1.0,  1.0, 1.0, 1.0], this.buffer);
 
-    let delta = this.size/200.0;
-
-    let angle_step = 360.0/this.segments;
-
-    for(var angle = 0; angle < 360; angle+=angle_step) {
-      let centerPT = [xy[0], xy[1]];
-      let angle1 = angle;
-      let angle2 = angle + angle_step;
-      let vec1 = [delta * Math.cos(angle1*Math.PI/180)/2, delta * Math.sin(angle1*Math.PI/180)/2];
-      let vec2 = [delta * Math.cos(angle2*Math.PI/180)/2, delta * Math.sin(angle2*Math.PI/180)/2];
-      let pt1 = [centerPT[0] + vec1[0], centerPT[1] + vec1[1]];
-      let pt2 = [centerPT[0] + vec2[0], centerPT[1] + vec2[1]];
-
-      drawTriangle([xy[0], xy[1], pt1[0], pt1[1], pt2[0], pt2[1]]);
-    }
+    // bottom face
+    gl.uniform4f(u_FragColor, 0.5*rgba[0], 0.5*rgba[1], 0.5*rgba[2], rgba[3]);
+    drawTriangle3D([0.0, 0.0, 0.0,  1.0, 0.0, 1.0,  1.0, 0.0, 0.0], this.buffer);
+    drawTriangle3D([0.0, 0.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0, 1.0], this.buffer);
+    
+    gl.uniform4f(u_FragColor, 0.75*rgba[0], 0.75*rgba[1], 0.75*rgba[2], rgba[3]);
+    // back of the cube
+    drawTriangle3D([0.0, 0.0, 1.0,  1.0, 1.0, 1.0,  1.0, 0.0, 1.0], this.buffer);
+    drawTriangle3D([0.0, 0.0, 1.0,  0.0, 1.0, 1.0,  1.0, 1.0, 1.0], this.buffer);
   }
 }
